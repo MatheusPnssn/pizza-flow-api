4ea5a137e04bc84925e7511f25d03ca98cbcc24d
Matheus Panisson da Silva <matheuspnssn@gmail.com>
2026-04-21 16:10:30 -0300

    sadsad

--- src/routes/index.js
+++ src/routes/index.js
@@ -27,7 +27,7 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
-app.use('/uploads', express.static('public/uploads'));
+router.use('/uploads', express.static('public/uploads'));
 
 /**
  * @swagger
